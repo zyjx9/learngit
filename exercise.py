476e6f071fcd785cb8eea292eba0a458c3be6044
Yijian <yijian.zhao@earlydata.com>
2026-01-06 16:15:34 +0800

    练习列表

--- exercise.py
+++ exercise.py
@@ -1,4 +1,4 @@
 s1 =72
 s2 =85 
 r =(s2 -s1) /s1 *100
-print(f'The score improved by {r:.1f}%')
+print(f'The score improved by {r:.1f}%;{s1:.02f} to {s2:.02f}')
---
+++ list_test.py
@@ -0,0 +1,8 @@
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Bob']
+]
+print(L[0][0])  # Apple
+print(L[1][1])  # Python
+print(L[2][2])  # Bob
