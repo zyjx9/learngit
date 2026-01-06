17da4974e2a246a104d5ee484afdda02b1a517f6
Yijian <yijian.zhao@earlydata.com>
2026-01-06 14:20:27 +0800

    练习f-string

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 def main():
     print('%2d-%02d' % (3, 1))
     print('%.2f' % 3.1415926)
-
+    r =2.5
+    s= 3.14 *r**2
+    print(f'The area of circle with radius {r} is {s:.2f}')
+    
 if __name__ == "__main__":
     main()
