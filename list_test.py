242e5d636dc9dff0f0f54332ff98e89f3b0dfc92
Yijian <yijian.zhao@earlydata.com>
2026-01-09 16:38:36 +0800

    添加列表生成式练习

--- list_test.py
+++ list_test.py
@@ -9,3 +9,14 @@ print(L[1][1])  # Python
 print(L[2][2])  # Bob
 
 print([d for d in os.listdir('.') ])
+
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
